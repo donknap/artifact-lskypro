e81552aebb1d9358fdd701823b8c1e00506a2064
donknap@gmail.com <donknap@gmail.com>
2023-11-21 21:46:28 +0800

    init

--- data/web/storage/framework/views/13233c99a325a4b9d6ec4c5f7e367cc2ce61a4ca.php
+++
@@ -1,2 +0,0 @@
-<span <?php echo e($attributes); ?>><?php echo e(\App\Utils::config(\App\Enums\ConfigKey::AppName)); ?></span>
-<?php /**PATH /home/site/resources/views/components/application-logo.blade.php ENDPATH**/ ?>
--- data/web/storage/framework/views/97f547050e7e2591e5b0e04feee7d80b57ecc922.php
+++
@@ -1,8 +0,0 @@
-<div class="rounded w-full overflow-hidden shadow-custom">
-    <div class="px-4 py-3 bg-slate-50 text-gray-800 text-md truncate"><?php echo e($title); ?></div>
-    <div class="bg-white w-full">
-        <?php echo e($content); ?>
-
-    </div>
-</div>
-<?php /**PATH /home/site/resources/views/components/box.blade.php ENDPATH**/ ?>
